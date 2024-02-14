e2b37926bd498057003b482dc77e928125711152
SanthoshkumarECE <103562246+SanthoshkumarECE@users.noreply.github.com>
2024-02-14 20:28:05 +0530

    Update handerc.py

--- handerc.py
+++ handerc.py
@@ -11,29 +11,42 @@ mpDraw = mp.solutions.drawing_utils
 pTime = 0
 cTime = 0
 
+tipIds = [4, 8, 12, 16, 20]  # Landmark indices for the tips of fingers
+
 while True:
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
-    #print(results.multi_hand_landmarks)
 
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
-            for id, lm in enumerate(handLms.landmark):
-                #print(id,lm)
-                h, w, c = img.shape
-                cx, cy = int(lm.x *w), int(lm.y*h)
-                #if id ==0:
-                cv2.circle(img, (cx,cy), 7, (255,0,255), cv2.FILLED)
-
+            # Draw landmarks
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
+            # Finger counting
+            fingers = []
+            # Thumb
+            if handLms.landmark[tipIds[0]].y < handLms.landmark[tipIds[0] - 1].y:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+            # Other fingers
+            for id in range(1, 5):
+                if handLms.landmark[tipIds[id]].y < handLms.landmark[tipIds[id] - 2].y:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
+
+            total_fingers = sum(fingers)
+            cv2.putText(img, f'Fingers: {total_fingers}', (10, 30),
+                        cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 255), 2)
 
     cTime = time.time()
-    fps = 1/(cTime-pTime)
+    fps = 1 / (cTime - pTime)
     pTime = cTime
 
-    cv2.putText(img,str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
+    cv2.putText(img, f'FPS: {int(fps)}', (10, 70),
+                cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
